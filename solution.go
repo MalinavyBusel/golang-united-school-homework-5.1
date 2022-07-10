6d02184c5f30de00550d73fc75d58cef0f6a652f
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2022-07-10 16:31:16 +0300

    init solution

---
+++ solution.go
@@ -0,0 +1,25 @@
+package task5_1
+
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	start Point
+	side  uint
+}
+
+func (s Square) End() (p Point) {
+	return Point{
+		x: s.start.x + int(s.side),
+		y: s.start.y + int(s.side),
+	}
+}
+
+func (s Square) Perimeter() (perimeter uint) {
+	return s.side * 4
+}
+
+func (s Square) Area() (area uint) {
+	return s.side * s.side
+}
